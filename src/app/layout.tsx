5d03e571b363b9484da031bb9b411cf7c273a978
Italo Doliva <italocoltact@gmail.com>
2025-07-31 10:41:53 -0300

    Tailwind variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`antialiased`}>
+    <html lang="en" className="h-dvh overflow-hidden">
+      <body className="antialiased font-grotesk h-full overflow-hidden text-black bg-white m-0 p-0 tracking-text">
         {children}
       </body>
     </html>
